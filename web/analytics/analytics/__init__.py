ce05860dd047922affc7daf221a4e8a273896961
Jamil Atta Junior <atta.jamil@gmail.com>
2011-09-02 11:43:06 -0300

    Add configure.zcml file 'Declarative Configuration' option from pyramid and code to add this file.

--- web/analytics/analytics/__init__.py
+++ web/analytics/analytics/__init__.py
@@ -1,12 +1,15 @@
 from pyramid.config import Configurator
 from analytics.resources import Root
+from pyramid.configuration import Configurator
+
+import pyramid_zcml
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(root_factory=Root, settings=settings)
-    config.add_view('analytics.views.chat_site',
-                    context='analytics:resources.Root',
-                    renderer='analytics:templates/chat_site.pt')
-    config.add_static_view('static', 'analytics:static')
+    config = Configurator()
+    config.begin()
+    config.include('pyramid_zcml')
+    config.load_zcml('configure.zcml')
+    config.end()
     return config.make_wsgi_app()
