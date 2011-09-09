d1958ea97c85567d0643566e11102033dc03d99f
Jamil Atta Junior <atta.jamil@gmail.com>
2011-09-10 04:06:50 +0800

    Add jsonp to use cross-domain

--- web/analytics/analytics/__init__.py
+++ web/analytics/analytics/__init__.py
@@ -2,6 +2,7 @@ from analytics.resources import Root
 from pyramid.configuration import Configurator
 from pyramid.events import subscriber
 from pyramid.events import NewRequest
+from pyramid.renderers import JSONP
 from pymongo import Connection
 
 import pyramid_zcml
@@ -18,9 +19,28 @@ def main(global_config, **settings):
     config.registry.settings['db_name'] = settings['db_name']
     config.add_subscriber(add_mongo_db, NewRequest)
 
-    config.include('pyramid_zcml')
-    config.load_zcml('configure.zcml')
-    config.end()
+    #config.include('pyramid_zcml')
+    #config.load_zcml('configure.zcml')
+    #config.end()
+    
+    config.add_renderer('jsonp', JSONP(param_name='callback'))
+    
+    #site
+    config.add_route('site', '/analytics/site/{instance}/{output}')
+    config.add_view('analytics.views.site', route_name='site', renderer='jsonp')
+
+    #site_option
+    config.add_route('site_option', '/analytics/site/{instance}/{option}/{output}')
+    config.add_view('analytics.views.site_option', route_name='site_option', renderer='jsonp')
+
+    #site_key
+    config.add_route('site_key', '/analytics/site/{instance}/{option}/{key}/{output}')
+    config.add_view('analytics.views.site_key', route_name='site_key', renderer='jsonp')
+
+    #site_option_range
+    config.add_route('site_option_range', '/analytics/site/{instance}/{option}/range/{range}/{output}')
+    config.add_view('analytics.views.site_option_range', route_name='site_option_range', renderer='jsonp')
+
     return config.make_wsgi_app()
 
 def add_mongo_db(event):
