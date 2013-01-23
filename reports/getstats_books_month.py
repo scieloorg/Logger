d88b19a10f7d7ddce53b3ea7ee7a247a0266c0d7
fabiobatalha <fabiobatalha@gmail.com>
2013-01-23 15:25:07 -0200

    Ajustes em script de tabulação de acessos para deixá-lo configurável quanto a ips de api e bases de dados

--- reports/getstats_books_month.py
+++ reports/getstats_books_month.py
@@ -3,10 +3,11 @@ from logaccess_config import *
 from pymongo import Connection
 import urllib2
 import json
+import argparse
 
 
-def get_books():
-    query1 = urllib2.urlopen('http://192.168.1.12:5984/scielobooks_1a/_design/scielobooks/_view/books')
+def get_books(api_host='localhost', api_port='5984'):
+    query1 = urllib2.urlopen('http://{0}:{1}/scielobooks_1a/_design/scielobooks/_view/books'.format(api_host, api_port))
     jsondocs = json.loads(query1.read())
 
     books = {}
@@ -47,33 +48,57 @@ def sumarize_by_month(stats):
 
     return sumarized
 
-books = get_books()
 
-conn = Connection('192.168.1.85', MONGODB_PORT)
+def get_collection(mongodb_host='localhost',
+               mongodb_port=27017,
+               mongodb_database='bks',
+               mongodb_collection='bks'):
 
-collections = ['bks']
+    conn = Connection(mongodb_host, mongodb_port)
+    db = conn[mongodb_database + "_accesslog"]
+    coll = db[mongodb_collection + "_analytics"]
+
+    return coll
 
 
-stats_dict = {}
-for coll_code in collections:
-    db = conn[coll_code + "_accesslog"]
-    analytics = db[coll_code + "_analytics"]
+def main(*args, **xargs):
+
+    books = get_books(api_host=xargs['api_host'],
+                      api_port=int(xargs['api_port']))
+
+    analytics = get_collection(mongodb_host=xargs['mongodb_host'],
+                               mongodb_port=int(xargs['mongodb_port']))
 
     books_stats = analytics.find({'context': 'book'})
 
+    stats_dict = {}
     for stat in books_stats:
         book_code = stat['code'].lower()
         stats_dict[book_code] = sumarize_by_month(stat)
 
-print "code|publicated at|created at|editor|access type|month access|accesses"
-for book_code, doc_types in stats_dict.items():
-    if book_code in books:
-        for doc_type, months in doc_types.items():
-            for month, accesses in months.items():
-                print u"{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(book_code,
-                                                        books[book_code]['year'],
-                                                        books[book_code]['creation_date'],
-                                                        books[book_code]['editor'],
-                                                        doc_type,
-                                                        month,
-                                                        accesses).encode("utf-8")
+    print "code|publicated at|created at|editor|access type|month access|accesses"
+    for book_code, doc_types in stats_dict.items():
+        if book_code in books:
+            for doc_type, months in doc_types.items():
+                for month, accesses in months.items():
+                    print u"{0}|{1}|{2}|{3}|{4}|{5}|{6}".format(book_code,
+                                                            books[book_code]['year'],
+                                                            books[book_code]['creation_date'],
+                                                            books[book_code]['editor'],
+                                                            doc_type,
+                                                            month,
+                                                            accesses).encode("utf-8")
+
+parser = argparse.ArgumentParser(description="Create an access report")
+parser.add_argument('--api_host', default='localhost', help='The CouchDB API hostname')
+parser.add_argument('--api_port', default='5984', help='The CouchDB API port')
+parser.add_argument('--mongodb_host', default='localhost', help='The MongoDB accesslog database hostname')
+parser.add_argument('--mongodb_port', default='27017', help='The MongoDB accesslog database port')
+
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    main(api_host=args.api_host,
+         api_port=args.api_port,
+         mongodb_host=args.mongodb_host,
+         mongodb_port=args.mongodb_port)
