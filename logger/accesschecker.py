2767b79e9e67c6f423b8d1f513967617e15310bc
Fabio Batalha <fabiobatalha@gmail.com>
2013-10-28 18:02:04 -0200

    Incluindo testes de instanciação da classe AccessChecker

--- logger/accesschecker.py
+++ logger/accesschecker.py
@@ -10,6 +10,7 @@ class AccessChecker(object):
             raise ValueError('Invalid collection id, you must select one of these %s' % str(allowed_collections)) 
 
         self.collection = collection
+        self.allowed_issns = self._allowed_issns()
 
     def _allowed_collections(self):        
         allowed_collections = []
@@ -24,3 +25,19 @@ class AccessChecker(object):
             allowed_collections.append(collection['id'])
         
         return allowed_collections
+
+    def _allowed_issns(self):        
+        query_url = 'http://webservices.scielo.org/scieloorg/_design/couchdb/_view/title_collectionstitle_keys?startkey=["%s",{}]&endkey=["%s"]&descending=true&limit=2000&include_docs=true' % (self.collection, self.collection)
+
+        try:
+            titles_json = urllib2.urlopen(query_url, timeout=3).read()
+        except urllib2.URLError:
+            raise urllib2.URLError('Was not possible to connect to webservices.scielo.org, try again later!')
+
+        titles = json.loads(titles_json)
+
+        title_dict = {}
+        for title in titles['rows']:
+            title_dict[title['doc']['v68'][0]['_']] = title['doc']['v400'][0]['_']
+
+        return title_dict
--- tests/test_requestchecker.py
+++ tests/test_requestchecker.py
@@ -1,67 +1,86 @@
 # coding: utf-8
 
+import urllib2
+
 from mocker import ANY, MockerTestCase
 
 from logger.accesschecker import AccessChecker
-
-network = """{"total_rows":31,"offset":0,"rows":[
-                {
-                    "id":"arg",
-                    "key":["certified","arg"],
-                    "value":{
-                        "_id":"arg",
-                        "_rev":"1-df50175ad35971784d6f713b9c5bbe5b",
-                        "acron":"arg",
-                        "acron2":"ar",
-                        "domain":"www.scielo.org.ar",
-                        "name":{
-                            "pt":"Argentina",
-                            "es":"Argentina",
-                            "en":"Argentina"},
-                        "status":"certified",
-                        "v706":"network"
-                    }
-                }]
-            }"""
+from . import fixtures
 
 class AccessCheckerTests(MockerTestCase):
 
     def test_instanciatingAccessChecker(self):
         mock_urllib2 = self.mocker.replace("urllib2")
-        mock_urlopen = self.mocker.mock()
 
-        mock_urllib2.urlopen(ANY)
-        self.mocker.result(mock_urlopen)
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.network)
 
-        mock_urlopen.read()
-        self.mocker.result(network)
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.titles)
+
         self.mocker.replay()
 
         self.assertTrue(isinstance(AccessChecker(collection='scl'), AccessChecker))
 
     def test_instanciatingAccessChecker_with_invalid_collection(self):
         mock_urllib2 = self.mocker.replace("urllib2")
-        mock_urlopen = self.mocker.mock()
 
-        mock_urllib2.urlopen(ANY)
-        self.mocker.result(mock_urlopen)
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.network)
 
-        mock_urlopen.read()
-        self.mocker.result(network)
         self.mocker.replay()
 
         with self.assertRaises(ValueError):
             AccessChecker(collection='xxx')
 
-    def test_allowed_collections(self):
+    def test_instanciatingAccessChecker_allowed_collections(self):
         mock_urllib2 = self.mocker.replace("urllib2")
-        mock_urlopen = self.mocker.mock()
 
-        mock_urllib2.urlopen(ANY)
-        self.mocker.result(mock_urlopen)
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.network)
 
-        mock_urlopen.read()
-        self.mocker.result(network)
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.titles)
+
         self.mocker.replay()
 
         self.assertEqual(AccessChecker(collection='scl').collection, 'scl')
+
+    def test_instanciatingAccessChecker_allowed_collections_exception(self):
+        mock_urllib2 = self.mocker.replace("urllib2")
+
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.throw(urllib2.URLError('Was not possible to connect to webservices.scielo.org, try again later!'))
+
+        self.mocker.replay()
+
+        with self.assertRaises(urllib2.URLError):
+            AccessChecker(collection='scl')
+
+    def test_instanciatingAccessChecker_allowed_issns(self):
+        mock_urllib2 = self.mocker.replace("urllib2")
+
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.network)
+
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.titles)
+
+        self.mocker.replay()
+
+        self.assertEqual(AccessChecker(collection='scl').allowed_issns, {u'bjmbr': u'1234-4321', u'zool': u'1984-4670'})
+
+    def test_instanciatingAccessChecker_allowed_issns_exception(self):
+        mock_urllib2 = self.mocker.replace("urllib2")
+
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.result(fixtures.network)
+
+        mock_urllib2.urlopen(ANY, timeout=3).read()
+        self.mocker.throw(urllib2.URLError('Was not possible to connect to webservices.scielo.org, try again later!'))
+
+        self.mocker.replay()
+
+        with self.assertRaises(urllib2.URLError):
+            AccessChecker(collection='scl')
+
