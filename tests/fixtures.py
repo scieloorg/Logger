f8c3e596b1222d0e10a3191766201bd23577a79a
fabiobatalha <fabiobatalha@gmail.com>
2014-01-28 16:50:28 -0200

    Incluindo mudanças para usar a api articlemeta como fonte de metadados de coleções e periódicos

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,24 +1,63 @@
-titles = """{"total_rows":1155,
-             "offset":136,
-             "rows":[
-                {
-                    "doc": {
-                        "v68": [{"_":"zool"}],
-                        "v400":[{"_":"1984-4670"}]
-                    }
-                },{
-                    "doc": {
-                        "v68": [{"_":"bjmbr"}],
-                        "v400":[{"_":"1234-4321"}]
-                    }
-                }
-             ]
-            }"""
+journals = """[
+    {
+        "v30": [{"_": "fbpe-3318"}],
+        "v64": [{"_": "bjournal@fmrp.usp.br"}],
+        "code": ["1414-431X", "0100-879X"],
+        "v901": [{"l": "es", "_": "Publicar los resultados de investigaciones originales que realicen aportes significativos en el \u00e1rea de las ciencias m\u00e9dicas y biol\u00f3gicas"}, {"l": "pt", "_": "Publicar resultados de pesquisas originais que contribuam significativamente para o conhecimento no campo das ci\u00eancias m\u00e9dicas e biol\u00f3gicas"}, {"l": "en", "_": "To publish the results of original research which contribute significantly to knowledge in medical and biological sciences"}], 
+        "v35": [{"_": "ONLIN"}], "v10": [{"_": "bjmbr"}],
+        "v935": [{"_": "1414-431X"}],
+        "v992": [{"_": "scl"}],
+        "v150": [{"_": "Braz J Med Biol Res"}],
+        "v151": [{"_": "Braz. j. med. biol. res"}],
+        "v930": [{"_": "BJMBR"}],
+        "v610": [{"_": "Revista brasileira de pesquisas m\u00e9dicas e biol\u00f3gicas"}],
+        "v440": [{"_": "MEDICINA"}, {"_": "BIOLOGIA GERAL"}],
+        "v441": [{"_": "Biological Sciences"}, {"_": "Health Sciences"}],
+        "v950": [{"_": "MRB"}],
+        "v951": [{"_": "MRB"}],
+        "v480": [{"_": "Associa\u00e7\u00e3o Brasileira de Divulga\u00e7\u00e3o Cient\u00edfica"}],
+        "v400": [{"_": "0100-879X"}],
+        "v420": [{"_": "BOF"}],
+        "v421": [{"_": "Braz J Med Biol Res"}],
+        "v350": [{"_": "en"}],
+        "v117": [{"_": "other"}],
+        "v310": [{"_": "BR"}],
+        "v330": [{"_": "CT"}],
+        "v85": [{"_": "nd"}],
+        "v691": [{"_": "100000000000000000000000"}],
+        "v690": [{"_": "www.scielo.br"}],
+        "v230": [{"_": "Revista brasileira de pesquisas m\u00e9dicas e biol\u00f3gicas"}],
+        "v854": [{"_": "BIOLOGY"}, {"_": "MEDICINE, RESEARCH & EXPERIMENTAL"}],
+        "v20": [{"_": "016281-7"}],
+        "v851": [{"_": "SCIE"}],
+        "v490": [{"_": "Ribeir\u00e3o Preto"}],
+        "v68": [{"_": "bjmbr"}],
+        "v67": [{"_": "na"}],
+        "v66": [{"_": "art"}],
+        "v51": [{"a": "19970424", "b": "C", "_": ""}],
+        "v63": [{"_": "Av. Bandeirantes, 3900"}, {"_": "14049-900 Ribeir\u00e3o Preto SP Brazil"}, {"_": "Tel. / Fax: +55 16 3633-3825"}],
+        "v62": [{"_": "Brazilian Journal of Medical and Biological Research"}],
+        "collection": "scl",
+        "v450": [{"_": "Current contents. Life sciences"}, {"_": "SciSearch"}, {"_": "Science citation index : an international interdisciplinary index to the literature"}, {"_": "Research Alert"}, {"_": "Index medicus (Washington. 1879)"}, {"_": "Chemical Abstracts Service"}, {"_": "Biological Abstracts"}, {"_": "Biosciences Information Service"}, {"_": "Excerpta Medica"}, {"_": "Index Medicus Latino-Americano"}, {"_": "ISI"}, {"_": "LILACS"}, {"_": "PubMed"}],
+        "v380": [{"_": "M"}],
+        "v6": [{"_": "ms"}],
+        "v900": [{"_": "ISSN impresso: 0100-879X"}],
+        "v943": [{"_": "20140121"}],
+        "v942": [{"_": "19970424"}],
+        "v941": [{"_": "20120824"}],
+        "v940": [{"_": "19970424"}],
+        "v340": [{"_": "B"}],
+        "v541": [{"_": "BY-NC"}],
+        "v430": [{"_": "QH W20.5"}],
+        "v5": [{"_": "S"}],
+        "v100": [{"_": "Brazilian Journal of Medical and Biological Research"}],
+        "v50": [{"_": "C"}],
+        "v320": [{"_": "SP"}],
+        "v360": [{"_": "en"}],
+        "v301": [{"_": "1981"}],
+        "v303": [{"_": "1"}],
+        "v302": [{"_": "14"}]
+    }, {"code": ["1984-4670"], "v901": [{"l": "es", "_": "Publicar contribuciones originales de investigadores, de todo el mundo, en las diversas l\u00edneas de especializaci\u00f3n abarcadas por la zoolog\u00eda: revisiones por invitaci\u00f3n, sistem\u00e1tica, taxonom\u00eda, nomenclatura, biogeograf\u00eda, morfolog\u00eda, biolog\u00eda, ecolog\u00eda, simbiosis, conservaci\u00f3n, comportamiento, gen\u00e9tica, zoolog\u00eda aplicada y \u00e1reas correlacionadas."}, {"l": "pt", "_": "Publicar contribui\u00e7\u00f5es originais de pesquisadores de todo o mundo nas diversas linhas de especialidade abrangidas pela zoologia: revis\u00f5es convidadas, sistem\u00e1tica, evolu\u00e7\u00e3o, taxonomia, nomenclatura, biogeografia, morfologia, fisiologia, biologia, ecologia, simbiose, conserva\u00e7\u00e3o, comportamento, gen\u00e9tica, zoologia aplicada e \u00e1reas correlatas."}, {"l": "en", "_": "To publish original contributions by researchers from any country on the various specialties encompassed by zoology: invited reviews, systematics, evolution, taxonomy, nomenclature, biogeography, morphology, physiology, biology, ecology, symbiosis, conservation, behavior, genetics, applied zoology and allied fields."}], "v35": [{"_": "PRINT"}], "v10": [{"_": "br1.1"}], "v935": [{"_": "1984-4670"}], "v992": [{"_": "scl"}], "v150": [{"_": "Zoologia (Curitiba)"}], "v151": [{"_": "Zoologia (Curitiba)"}], "v930": [{"_": "ZOOL"}], "v610": [{"_": "Revista Brasileira de Zoologia"}], "v440": [{"_": "ZOOLOGIA"}, {"_": "MORFOLOGIA DOS GRUPOS RECENTES"}, {"_": "FISIOLOGIA DOS GRUPOS RECENTES"}, {"_": "COMPORTAMENTO ANIMAL"}, {"_": "TAXONOMIA DOS GRUPOS RECENTES"}, {"_": "ZOOLOGIA APLICADA"}, {"_": "CONSERVA\u00c7\u00c3O DAS ESP\u00c9CIES ANIMAIS"}, {"_": "ECOLOGIA"}, {"_": "ECOLOGIA DE ECOSSISTEMAS"}, {"_": "ANATOMIA ANIMAL"}, {"_": "PARASITOLOGIA"}, {"_": "PROTOZOOLOGIA PARASIT\u00c1RIA ANIMAL"}, {"_": "HELMINTOLOGIA DE PARASITOS"}, {"_": "HELMINTOLOGIA ANIMAL"}, {"_": "ENTOMOLOGIA E MALACOLOGIA DE PARASITOS E VETORES"}], "v441": [{"_": "Biological Sciences"}], "v950": [{"_": "EMS"}], "v951": [{"_": "EMS"}], "v480": [{"_": "Sociedade Brasileira de Zoologia"}], "v400": [{"_": "1984-4670"}], "v350": [{"_": "en"}, {"_": "pt"}], "v117": [{"_": "other"}], "v310": [{"_": "BR"}], "v330": [{"_": "CT"}], "v85": [{"_": "nd"}], "v692": [{"_": "http://mc04.manuscriptcentral.com/zool-scielo"}], "v691": [{"_": "100000000000000000000000"}], "v854": [{"_": "ZOOLOGY"}], "v64": [{"_": "sbz@sbzoologia.org.br"}], "v851": [{"_": "SCIE"}], "v490": [{"_": "Curitiba"}], "v68": [{"_": "zool"}], "v67": [{"_": "na"}], "v66": [{"_": "art"}], "v65": [{"_": "<hr>"}], "v51": [{"a": "20090505", "b": "C", "_": ""}], "v63": [{"_": "Caixa Postal 19020"}, {"_": "81531-980 Curitiba PR Brasil"}, {"_": "Tel./Fax: (55 41) 3266-6823"}], "v62": [{"_": "Sociedade Brasileira de Zoologia"}], "collection": "scl", "v450": [{"_": "ASFA-FAO"}, {"_": "Biological Abstracts"}, {"_": "CAB International"}, {"_": "Institute for Scientific Information (ISI)"}, {"_": "Scopus (Elsevier)"}, {"_": "Zoological Record"}, {"_": ""}], "v140": [{"_": "Conselho Nacional de Desenvolvimento Cient\u00edfico e Tecnol\u00f3gico (CNPq)"}, {"_": "Coordena\u00e7\u00e3o de Aperfei\u00e7oamento de Pessoal de N\u00edvel Superior (CAPES)"}, {"_": "Sociedade Brasileira de Zoologia (SBZ)"}], "v380": [{"_": "Q"}], "v6": [{"_": "c"}], "v943": [{"_": "20140121"}], "v942": [{"_": "20090327"}], "v941": [{"_": "20131217"}], "v940": [{"_": "20090327"}], "v340": [{"_": "B"}], "v541": [{"_": "BY-NC"}], "v5": [{"_": "S"}], "v100": [{"_": "Zoologia (Curitiba)"}], "v50": [{"_": "C"}], "v320": [{"_": "PR"}], "v240": [{"_": "Zoologia (Curitiba)"}], "v360": [{"_": "en"}], "v301": [{"_": "2009"}], "v303": [{"_": "1"}], "v302": [{"_": "26"}]}]"""
 
-network = """{"total_rows":31,"offset":0,"rows":[
-                {
-                    "id":"scl"
-                },{
-                    "id":"arg"
-                }]
-            }"""
+collections = """
+[{"acron": "arg", "status": "certified", "domain": "www.scielo.org.ar", "code": "arg", "name": {"en": "Argentina", "pt": "Argentina", "es": "Argentina"}, "acron2": "ar"}, {"acron": "chl", "status": "certified", "domain": "www.scielo.cl", "code": "chl", "name": {"en": "Chile", "pt": "Chile", "es": "Chile"}, "acron2": "cl"}, {"acron": "col", "status": "certified", "domain": "www.scielo.org.co", "code": "col", "name": {"en": "Colombia", "pt": "Colombia", "es": "Colombia"}, "acron2": "co"}, {"acron": "cub", "status": "certified", "domain": "scielo.sld.cu", "code": "cub", "name": {"en": "Cuba", "pt": "Cuba", "es": "Cuba"}, "acron2": "cu"}, {"acron": "esp", "status": "certified", "domain": "scielo.isciii.es", "code": "esp", "name": {"en": "Spain", "pt": "Espanha", "es": "Espa\u00f1a"}, "acron2": "es"}, {"acron": "mex", "status": "certified", "domain": "www.scielo.org.mx", "code": "mex", "name": {"en": "Mexico", "pt": "Mexico", "es": "Mexico"}, "acron2": "mx"}, {"acron": "prt", "status": "certified", "domain": "www.scielo.oces.mctes.pt", "code": "prt", "name": {"en": "Portugal", "pt": "Portugal", "es": "Portugal"}, "acron2": "pt"}, {"acron": "scl", "status": "certified", "domain": "www.scielo.br", "code": "scl", "name": {"en": "Brazil", "pt": "Brasil", "es": "Brasil"}, "acron2": "br"}, {"acron": "spa", "status": "certified", "domain": "www.scielosp.org", "code": "spa", "name": {"en": "Public Health", "pt": "Sa\u00fade P\u00fablica", "es": "Salud Publica"}, "acron2": "sp"}, {"acron": "sss", "status": "certified", "domain": "www.scielo.org.mx", "code": "sss", "name": {"en": "Social Sciences", "pt": "Social Sciences", "es": "Social Sciences"}, "acron2": "ss"}, {"acron": "sza", "status": "certified", "domain": "www.scielo.org.za", "code": "sza", "name": {"en": "South Africa", "pt": "\u00c1frica do Sul", "es": "Sud\u00e1frica"}, "acron2": "za"}, {"acron": "ven", "status": "certified", "domain": "www.scielo.org.ve", "code": "ven", "name": {"en": "Venezuela", "pt": "Venezuela", "es": "Venezuela"}, "acron2": "ve"}, {"acron": "bio", "status": "development", "domain": "biodiversidade.scielo.br", "code": "bio", "name": {"en": "Biodiversidade", "pt": "Biodiversidade", "es": "Biodiversidade"}, "acron2": "bi"}, {"acron": "bol", "status": "development", "domain": "www.scielo.org.bo", "code": "bol", "name": {"en": "Bolivia", "pt": "Bolivia", "es": "Bolivia"}, "acron2": "bo"}, {"acron": "cri", "status": "development", "domain": "www.scielo.sa.cr", "code": "cri", "name": {"en": "Costa Rica", "pt": "Costa Rica", "es": "Costa Rica"}, "acron2": "cr"}, {"acron": "per", "status": "development", "domain": "www.scielo.org.pe", "code": "per", "name": {"en": "Peru", "pt": "Peru", "es": "Peru"}, "acron2": "pe"}, {"acron": "pro", "status": "development", "domain": "www.proceedings.scielo.br", "code": "pro", "name": {"en": "Proceedings", "pt": "Proceedings", "es": "Proceedings"}, "acron2": "pro"}, {"acron": "pry", "status": "development", "domain": "scielo.iics.una.py", "code": "pry", "name": {"en": "Paraguay", "pt": "Paraguai", "es": "Paraguay"}, "acron2": "py"}, {"acron": "ury", "status": "development", "domain": "www.scielo.edu.uy", "code": "ury", "name": {"en": "Uruguay", "pt": "Uruguai", "es": "Uruguay"}, "acron2": "uy"}, {"acron": "wid", "status": "development", "domain": "caribbean.scielo.org", "code": "wid", "name": {"en": "West Indians", "pt": "West Indians", "es": "West Indians"}, "acron2": "wi"}, {"acron": "cci", "status": "diffusion", "domain": "comciencia.scielo.br", "code": "cci", "name": {"en": "ComCi\u00eancia", "pt": "ComCi\u00eancia", "es": "ComCi\u00eancia"}, "acron2": "cc"}, {"acron": "cic", "status": "diffusion", "domain": "cienciaecultura.bvs.br", "code": "cic", "name": {"en": "Ci\u00eancia e Cultura", "pt": "Ci\u00eancia e Cultura", "es": "Ci\u00eancia e Cultura"}, "acron2": "ci"}, {"acron": "inv", "status": "diffusion", "domain": "inovacao.scielo.br", "code": "inv", "name": {"en": "Conhecimento e Inova\u00e7\u00e3o", "pt": "Conhecimento e Inova\u00e7\u00e3o", "es": "Conhecimento e Inova\u00e7\u00e3o"}, "acron2": "cinov"}, {"acron": "pef", "status": "diffusion", "domain": "revistapesquisa.fapesp.br", "code": "pef", "name": {"en": "Pesquisa FAPESP", "pt": "Pesquisa FAPESP", "es": "Pesquisa FAPESP"}, "acron2": "pef"}, {"acron": "rvq", "status": "diffusion", "domain": "www.uff.br/RVQ", "code": "rvq", "name": {"en": "Revista Virtual de Qu\u00edmica", "pt": "Revista Virtual de Qu\u00edmica", "es": "Revista Virtual de Qu\u00edmica"}, "acron2": "rvq"}, {"acron": "edc", "status": "independent", "domain": "educa.fcc.org.br", "code": "edc", "name": {"en": "Educa", "pt": "Educa", "es": "Educa"}, "acron2": "educa"}, {"acron": "ppg", "status": "independent", "domain": "ppegeo.igc.usp.br", "code": "ppg", "name": {"en": "Portal de Peri\u00f3dicos Eletr\u00f4nicos em Geoci\u00eancias", "pt": "Portal de Peri\u00f3dicos Eletr\u00f4nicos em Geoci\u00eancias", "es": "Portal de Peri\u00f3dicos Eletr\u00f4nicos em Geoci\u00eancias"}, "acron2": "ppegeo"}, {"acron": "psi", "status": "independent", "domain": "pepsic.bvsalud.org", "code": "psi", "name": {"en": "Peri\u00f3dicos Eletr\u00f4nicos em Psicologia", "pt": "Peri\u00f3dicos Eletr\u00f4nicos em Psicologia", "es": "Peri\u00f3dicos Eletr\u00f4nicos em Psicologia"}, "acron2": "pepsic"}, {"acron": "rve", "status": "independent", "domain": "www.revenf.bvs.br", "code": "rve", "name": {"en": "Revista de Enfermagem", "pt": "Revista de Enfermagem", "es": "Revista de Enfermagem"}, "acron2": "revenf"}, {"acron": "rvo", "status": "independent", "domain": "revodonto.bvsalud.org", "code": "rvo", "name": {"en": "RevOdonto", "pt": "RevOdonto", "es": "RevOdonto"}, "acron2": "revodonto"}, {"acron": "ses", "status": "independent", "domain": "periodicos.ses.sp.bvs.br", "code": "ses", "name": {"en": "Portal de Revistas - SES", "pt": "Portal de Revistas - SES", "es": "Portal de Revistas - SES"}, "acron2": "ses"}]"""
