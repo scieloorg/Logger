d3a93d8244ce68880ca9db345446f687c66891eb
fabiobatalha <fabiobatalha@gmail.com>
2014-01-29 16:42:33 -0200

    Ajustes na ferramenta de registro de logs

--- logger/loadlogs.py
+++ logger/loadlogs.py
@@ -1,10 +1,61 @@
 #!/usr/bin/env python
+# coding: utf-8
+import os
 import argparse
 
-from tools import *
+from pymongo import Connection
+
 from ratchet import *
-from logaccess_config import *
 from accesschecker import AccessChecker, TimedSet, checkdatelock
+
+def get_proc_collection():
+    """
+    The proc collection is a mongodb database that keeps the name of each
+    processed file, to avoid processing these files again.
+    """
+    conn = Connection(LOGGER_DATABASE_DOMAIN, LOGGER_DATABASE_PORT)
+    db = conn['proc_files']
+    coll = db[LOGGER_DATABASE_COLLECTION]
+    coll.ensure_index('file_name')
+
+    return coll
+
+
+def get_proc_robots_collection():
+    """
+    The robots collection is a mongodb database that keeps the count of each
+    robots occurences record for each url + ip address.
+    """
+    conn = Connection(LOGGER_DATABASE_DOMAIN, LOGGER_DATABASE_PORT)
+    db = conn['robots']
+    coll = db[LOGGER_DATABASE_COLLECTION]
+    coll.ensure_index('code')
+
+    return coll
+
+
+def register_pdf_download_accesses(ratchet_queue, issn, pdfid, date, ip):
+    ratchet_queue.register_download_access(pdfid, ','.join(issn), date)
+
+
+def register_html_accesses(ratchet_queue, script, pid, date, ip):
+
+    if script == "sci_serial":
+        ratchet_queue.register_journal_access(','.join(pid), date)
+    elif script == "sci_abstract":
+        ratchet_queue.register_abstract_access(pid, date)
+    elif script == "sci_issuetoc":
+        ratchet_queue.register_toc_access(pid, date)
+    elif script == "sci_arttext":
+        ratchet_queue.register_article_access(pid, date)
+    elif script == "sci_pdf":
+        ratchet_queue.register_pdf_access(pid, date)
+    elif script == "sci_home":
+        ratchet_queue.register_home_access(pid, date)
+    elif script == "sci_issues":
+        ratchet_queue.register_issues_access(pid, date)
+    elif script == "sci_alphabetic":
+        ratchet_queue.register_alpha_access(pid, date)
 
 
 def main(*args, **xargs):
@@ -13,52 +64,55 @@ def main(*args, **xargs):
     proc_coll = get_proc_collection()
     proc_robots_coll = get_proc_robots_collection()
 
-    rq = RatchetQueue(limit=100)
-    for logfile in get_files_in_logdir(LOG_DIR):
+    for logfile in os.popen('ls %s/*' % LOG_DIR):
 
-        if log_was_processed(proc_coll, logfile):
+        logfile = logfile.strip()
+
+        # Verifica se arquivo já foi processado.
+        if proc_coll.find({'file_name': logfile}).count() > 0:
             continue
 
+        # Registra em base de dados de arquivos processados o novo arquivo.
         print "processing: {0}".format(logfile)
-        reg_logfile(proc_coll, logfile)
+        proc_coll.insert({'file_name': logfile})
 
-        for raw_line in get_file_lines(logfile):
-            parsed_line = ac.parsed_access(raw_line)
+        with open(logfile, 'rb') as f:
+            rq = RatchetQueue(error_log_file=xargs['error_log_file'])
+            for raw_line in f:
+                parsed_line = ac.parsed_access(raw_line)
 
-            if not parsed_line:
-                continue
-
-            locktime = 10
-            if parsed_line['access_type'] == "PDF":
-                locktime = 30
-
-            if xargs['ttl']:
-                try:
-                    lockid = parsed_line['code']+parsed_line['script']
-                    ts.add(lockid,
-                           parsed_line['iso_datetime'], locktime)
-                except ValueError:
+                if not parsed_line:
                     continue
 
-            if parsed_line['access_type'] == "PDF":
-                pdfid = parsed_line['pdf_path']
-                issn = parsed_line['pdf_issn']
-                register_pdf_download_accesses(rq,
-                                               issn,
-                                               pdfid,
-                                               parsed_line['iso_date'],
-                                               parsed_line['ip'])
+                locktime = 10
+                if parsed_line['access_type'] == "PDF":
+                    locktime = 30
 
-            if parsed_line['access_type'] == "HTML":
-                script = parsed_line['query_string']['script']
-                pid = parsed_line['query_string']['pid']
-                register_html_accesses(rq,
-                                       script,
-                                       pid,
-                                       parsed_line['iso_date'],
-                                       parsed_line['ip'])
+                if xargs['ttl']:
+                    try:
+                        lockid = parsed_line['code']+parsed_line['script']
+                        ts.add(lockid,
+                               parsed_line['iso_datetime'], locktime)
+                    except ValueError:
+                        continue
 
-        rq.send()
+                if parsed_line['access_type'] == "PDF":
+                    pdfid = parsed_line['pdf_path']
+                    issn = parsed_line['pdf_issn']
+                    register_pdf_download_accesses(rq,
+                                                   issn,
+                                                   pdfid,
+                                                   parsed_line['iso_date'],
+                                                   parsed_line['ip'])
+
+                if parsed_line['access_type'] == "HTML":
+                    script = parsed_line['query_string']['script']
+                    pid = parsed_line['query_string']['pid']
+                    register_html_accesses(rq,
+                                           script,
+                                           pid,
+                                           parsed_line['iso_date'],
+                                           parsed_line['ip'])
 
 if __name__ == '__main__':
 
@@ -72,6 +126,12 @@ if __name__ == '__main__':
                         '--collection',
                         default=None,
                         help='Three letters collection id')
+    parser.add_argument('-l',
+                        '--error_log_file',
+                        default=None,
+                        help='error log filename')
     args = parser.parse_args()
 
-    main(ttl=bool(args.ttl), collection=str(args.collection))
+    main(ttl=bool(args.ttl),
+         collection=str(args.collection),
+         error_log_file=str(args.error_log_file))
