bdf9c84b28c18a1631f1c9e58a09c8b328afa794
fabiobatalha <fabiobatalha@gmail.com>
2015-10-08 17:07:51 -0300

    Ajustes em tasks

--- logger/tasks.py
+++ logger/tasks.py
@@ -1,5 +1,4 @@
 from celery import Celery
-import cProfile, pstats
 
 from logger import scielo
 import utils
@@ -13,15 +12,8 @@ app = Celery('tasks', broker=celery_broker)
 
 @app.task
 def readlog(logfile, collection):
-    pr = cProfile.Profile()
-    pr.enable()
-
-    file_profile = open('profile.txt', 'w')
     with scielo.Bulk(collection) as bk:
         bk.read_log(logfile)
         os.remove(logfile)
-
-    pr.disable()
-    ps = pstats.Stats(pr, stream=file_profile).sort_stats('cumulative')
 
     return 'read log: %s' % logfile
