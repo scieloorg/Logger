d19cdcffd45f01d19fecf2bf40c0e2c90acec8dd
fabiobatalha <fabiobatalha@gmail.com>
2016-08-26 15:42:32 -0300

    Ajuste para carga de logs do ReadCube

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(here, 'CHANGES.rst')) as f:
 
 install_requires = [
     'requests>=2.6.0',
-    'apachelog>=1.1',
+    'apachelog>=1.0',
     'pymongo>=2.8',
     'celery>=3.1.18',
     'watchdog>=0.8.3'
@@ -26,7 +26,7 @@ tests_require = [
 
 setup(
     name="logger",
-    version='0.2.12',
+    version='0.3.12',
     description="A SciELO tool to load apache log files and register access into Ratchet.",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
