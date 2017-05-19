8c15b9ed4eccdc923726b8c20efeaec4c9cbd002
fabiobatalha <fabiobatalha@gmail.com>
2017-05-19 10:12:28 -0300

    Ajustes em Inspector

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name="logger",
-    version='1.6.13',
+    version='1.7.13',
     description="A SciELO tool to load apache log files and register access into Ratchet.",
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
