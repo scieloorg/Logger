d9ca8fa2e49b42ef2552346b85e6f3ce8431b720
Roberta Takenaka <roberta.takenaka@scielo.org>
2021-10-28 16:37:48 -0300

    Move bloco relacionado com a variável settings para o topo

--- logger/utils.py
+++ logger/utils.py
@@ -14,6 +14,68 @@ from ConfigParser import SafeConfigParser
 logger = logging.getLogger(__name__)
 
 
+class SingletonMixin(object):
+    """
+    Adds a singleton behaviour to an existing class.
+
+    weakrefs are used in order to keep a low memory footprint.
+    As a result, args and kwargs passed to classes initializers
+    must be of weakly refereable types.
+    """
+    _instances = weakref.WeakValueDictionary()
+
+    def __new__(cls, *args, **kwargs):
+        key = (cls, args, tuple(kwargs.items()))
+
+        if key in cls._instances:
+            return cls._instances[key]
+
+        new_instance = super(type(cls), cls).__new__(cls, *args, **kwargs)
+        cls._instances[key] = new_instance
+
+        return new_instance
+
+
+class Configuration(SingletonMixin):
+    """
+    Acts as a proxy to the ConfigParser module
+    """
+    def __init__(self, fp, parser_dep=SafeConfigParser):
+        self.conf = parser_dep()
+        self.conf.readfp(fp)
+
+    @classmethod
+    def from_env(cls):
+        try:
+            filepath = os.environ['LOGGER_SETTINGS_FILE']
+        except KeyError:
+            raise ValueError('missing env variable LOGGER_SETTINGS_FILE')
+
+        return cls.from_file(filepath)
+
+    @classmethod
+    def from_file(cls, filepath):
+        """
+        Returns an instance of Configuration
+
+        ``filepath`` is a text string.
+        """
+        fp = open(filepath, 'rb')
+        return cls(fp)
+
+    def __getattr__(self, attr):
+        return getattr(self.conf, attr)
+
+    def items(self):
+        """Settings as key-value pair.
+        """
+        return [(section, dict(self.conf.items(section, raw=True))) for \
+            section in [section for section in self.conf.sections()]]
+
+
+settings = dict(Configuration.from_env().items())['app:main']
+
+
 def try_get_collections(am_client):
     for i in (1, 2, 5, 10):
         try:
@@ -110,67 +172,6 @@ class Collections(object):
 
     def codes(self):
         return self.indexed_by_code.keys()
-
-
-class SingletonMixin(object):
-    """
-    Adds a singleton behaviour to an existing class.
-
-    weakrefs are used in order to keep a low memory footprint.
-    As a result, args and kwargs passed to classes initializers
-    must be of weakly refereable types.
-    """
-    _instances = weakref.WeakValueDictionary()
-
-    def __new__(cls, *args, **kwargs):
-        key = (cls, args, tuple(kwargs.items()))
-
-        if key in cls._instances:
-            return cls._instances[key]
-
-        new_instance = super(type(cls), cls).__new__(cls, *args, **kwargs)
-        cls._instances[key] = new_instance
-
-        return new_instance
-
-
-class Configuration(SingletonMixin):
-    """
-    Acts as a proxy to the ConfigParser module
-    """
-    def __init__(self, fp, parser_dep=SafeConfigParser):
-        self.conf = parser_dep()
-        self.conf.readfp(fp)
-
-    @classmethod
-    def from_env(cls):
-        try:
-            filepath = os.environ['LOGGER_SETTINGS_FILE']
-        except KeyError:
-            raise ValueError('missing env variable LOGGER_SETTINGS_FILE')
-
-        return cls.from_file(filepath)
-
-    @classmethod
-    def from_file(cls, filepath):
-        """
-        Returns an instance of Configuration
-
-        ``filepath`` is a text string.
-        """
-        fp = open(filepath, 'rb')
-        return cls(fp)
-
-    def __getattr__(self, attr):
-        return getattr(self.conf, attr)
-
-    def items(self):
-        """Settings as key-value pair.
-        """
-        return [(section, dict(self.conf.items(section, raw=True))) for \
-            section in [section for section in self.conf.sections()]]
-
-settings = dict(Configuration.from_env().items())['app:main']
 
 
 def checkdatelock(previous_date=None, next_date=None, locktime=10):
